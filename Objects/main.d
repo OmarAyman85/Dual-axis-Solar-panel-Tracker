7784e1e7ec80a31043b01efb4555519deaa0d7e9
OmarAyman85 <oamr.shivy85@gmail.com>
2024-12-09 23:17:14 +0200

    Initial commit

---
+++ Objects/main.d
@@ -0,0 +1,29 @@
+./objects/main.o: main.c C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdbool.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\ti\TivaWare_C_Series-2.2.0.295\inc\hw_memmap.h \
+  C:\ti\TivaWare_C_Series-2.2.0.295\inc\hw_types.h \
+  C:\ti\TivaWare_C_Series-2.2.0.295\inc\hw_gpio.h \
+  C:\ti\TivaWare_C_Series-2.2.0.295\driverlib\adc.h \
+  C:\ti\TivaWare_C_Series-2.2.0.295\driverlib\gpio.h \
+  C:\ti\TivaWare_C_Series-2.2.0.295\driverlib\pin_map.h \
+  C:\ti\TivaWare_C_Series-2.2.0.295\driverlib\sysctl.h \
+  C:\ti\TivaWare_C_Series-2.2.0.295\driverlib\debug.h \
+  C:\ti\TivaWare_C_Series-2.2.0.295\driverlib\pwm.h \
+  C:\ti\TivaWare_C_Series-2.2.0.295\driverlib\timer.h \
+  C:\ti\TivaWare_C_Series-2.2.0.295\inc\tm4c123gh6pm.h \
+  C:\Users\Omar\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.4.6\Source\include\FreeRTOS.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h \
+  RTE\RTOS\FreeRTOSConfig.h \
+  C:\Users\Omar\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.4.6\CMSIS\RTOS2\FreeRTOS\Include\freertos_evr.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  RTE\_Target_1\RTE_Components.h \
+  C:\Users\Omar\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.4.6\Source\include\projdefs.h \
+  C:\Users\Omar\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.4.6\Source\include\portable.h \
+  C:\Users\Omar\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.4.6\Source\include\deprecated_definitions.h \
+  C:\Users\Omar\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.4.6\Source\portable\GCC\ARM_CM4F\portmacro.h \
+  C:\Users\Omar\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.4.6\Source\include\mpu_wrappers.h \
+  C:\Users\Omar\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.4.6\Source\include\task.h \
+  C:\Users\Omar\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.4.6\Source\include\list.h \
+  C:\Users\Omar\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.4.6\Source\include\timers.h \
+  C:\Users\Omar\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.4.6\Source\include\queue.h \
+  C:\Users\Omar\AppData\Local\Arm\Packs\ARM\CMSIS-FreeRTOS\10.4.6\Source\include\semphr.h
